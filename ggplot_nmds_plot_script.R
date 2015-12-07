d6dd814e4b8736fc7805b1ab41fbda2097414fc0
Fan Yang <fanyang@fans-mbp.eeob.iastate.edu>
2015-12-07 16:00:02 -0600

    ggplot_nmds plot script

---
+++ ggplot_nmds_plot_script.R
@@ -0,0 +1,87 @@
+## script is written for pitfoaming arisa data currently
+
+library(vegan)
+library(ggplot2)
+library(RColorBrewer)
+
+## RyanJW's nmds plot function:
+##NMDS for 16S
+ggplot.NMDS<-function(XX,ZZ,COLORS){
+	library(ggplot2)
+MDS1<-data.frame(scores(XX))$NMDS1
+MDS2<-data.frame(scores(XX))$NMDS2
+Treatment<-ZZ
+
+NMDS<-data.frame(MDS1,MDS2,Treatment)
+
+NMDS.mean=aggregate(NMDS[,1:2],list(group=Treatment),mean)
+
+veganCovEllipse<-function (cov, center = c(0, 0), scale = 1, npoints = 100) 
+  {
+    theta <- (0:npoints) * 2 * pi/npoints
+    Circle <- cbind(cos(theta), sin(theta))
+    t(center + scale * t(Circle %*% chol(cov)))
+  }
+
+  df_ell <- data.frame()
+  for(g in levels(NMDS$Treatment)){
+    df_ell <- rbind(df_ell, cbind(as.data.frame(with(NMDS[NMDS$Treatment==g,],
+                    veganCovEllipse(cov.wt(cbind(MDS1,MDS2),wt=rep(1/length(MDS1),length(MDS1)))$cov,center=c(mean(MDS1),mean(MDS2)))))
+                    ,group=g))
+  }
+
+X1<-ggplot(data = NMDS, aes(MDS1, MDS2)) + geom_point(aes(color = Treatment),size=3,alpha=0.75) +
+    geom_path(data=df_ell, aes(x=MDS1, y=MDS2,colour=group), size=2, linetype=5)+theme_bw()+theme(aspect.ratio=1)+scale_color_manual(values=COLORS)+theme(axis.text.x=element_text(size=20),axis.text.y=element_text(size=20),axis.title.x=element_text(size=20),axis.title.y=element_text(size=20))+theme(legend.title=element_text(size=15),legend.text=element_text(size=15))
+X1    
+}
+
+
+args<-commandArgs(TRUE)
+
+### Dan's arisa data,  read in needs to have samples as rows, otu's as columns. 
+#data<-read.delim(args[1], sep="\t", header=T)
+#data.trans<-decostand(data[, -1:-3], "total")
+#data.mds<-metaMDS(data.trans, k=3, autotransform=F)
+#
+#colors_fiber<-c("red", "green", "blue")
+#colors_size<-c("red", "blue")
+#colors_combined<-c("red", "blue", "green", "black", "purple", "grey")
+#
+#data$combined<-paste(data[, 2], data[, 3], sep="_")
+#pdf("arisa_nmds_combined.pdf")
+#ggplot.NMDS(data.mds, data$combined, colors_combined)
+#dev.off()
+#
+##pdf("arisa_nmds_fiber.pdf")
+##ggplot.NMDS(data.mds, data$Fiber, colors_fiber)
+##dev.off()
+#
+##pdf("arisa_nmds_grindSize.pdf")
+##ggplot.NMDS(data.mds, data[,2], colors_size)
+##dev.off()
+#
+##"Fiber Adonis:"
+##adonis(data.trans~data$Fiber)
+##
+##"Grind size Adonis:"
+##adonis(data.trans~data[,2])
+#
+#"size_fiber Adonis:"
+#adonis(data.trans~data[,2]*data[,3])
+
+## Angela's arisa data,  read in needs to have samples as rows, otu's as columns. 
+data<-read.delim(args[1], sep="\t", header=T)
+data<-data[rowSums(is.na(data[, -1:-9]))==0,] ##remove rows with NA's
+data.trans<-decostand(data[, -1:-9], "total")
+data.mds<-metaMDS(data.trans, k=3, autotransform=F)
+
+for (i in names(data[, 3:9])){
+	print(c("Processing treatment:", i, "!!!!!!!!!!!!!!!!"))
+	colorCount = length(unique(data[, i]))
+	getPalette = colorRampPalette(brewer.pal(8, "Dark2"))
+	colors = getPalette(colorCount)
+	pdf(paste("arisa_nmds_",i,".pdf", sep=""))
+	ggplot.NMDS(data.mds, data[, i], colors)
+	dev.off()
+	adonis(data.trans~data[,i])
+} 
