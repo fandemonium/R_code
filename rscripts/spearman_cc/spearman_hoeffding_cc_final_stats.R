a4882563cef4519add81e2e8a09a0c95079d27c9
Fan Yang <fanyang@fans-mbp.eeob.iastate.edu>
2016-04-27 16:43:04 -0500

    updated network stats and added r function on how to calculate effective size

--- rscripts/spearman_cc/spearman_hoeffding_cc_final_stats.R
+++ rscripts/spearman_cc/spearman_hoeffding_cc_final_stats.R
@@ -5,23 +5,19 @@ library(reshape2)
 library(igraph)
 library(fdrtool)
 
+source("~/Documents/repos/R_code/R_functions/network_effective_size.R")
+
 ### input args: 1. phyloseq RDS object; 2. the last column that is a factor; 
 args<-commandArgs(TRUE)
 input_path<-unlist(strsplit(as.character(args[1]), "/", fixed=T))
 full_name<-input_path[length(input_path)]
 input_name<-strsplit(as.character(full_name), ".", fixed=T)
 
-final_results<-read.delim(args[1], sep="\t", header=T)
+input_results<-read.delim(args[1], sep="\t", header=T)
 
 ##barn_foaming.rate<-read.delim("barn_foaming_rate.txt", sep="\t", header=T)
 barn_foaming.rate<-read.delim(args[2], sep="\t", header=T)
-#final_results<-merge(final_results, barn_foaming.rate[, c("id", "category")], "id")
-#final_results$Var1<-paste(final_results$Var1, final_results$category, sep="::")
-#final_results$Var2<-paste(final_results$Var2, final_results$category, sep="::")
-#final_results<-data.frame(final_results[, c("Var1", "Var2", "id")], final_results[, 4:length(final_results[1,])])
-final_results<-subset(final_results, D >= "0.65")
-final_results$Var1<-paste(final_results$Var1, final_results$id, sep="::")
-final_results$Var2<-paste(final_results$Var2, final_results$id, sep="::")
+final_results<-subset(input_results, D >= "0.65")
 final_results<-data.frame(final_results[, c("Var1", "Var2", "id")], final_results[, 4:length(final_results[1,])])
 
 # now we can calculate stats for the network
@@ -32,7 +28,26 @@ for(i in unique(final_results$id)){
         E(temp.graph)$weight<-abs(temp$rho)
         temp.graph<-simplify(temp.graph)
         id <- i
-        clustering_coeff<-transitivity(temp.graph)
+	
+	N_nodes<-vcount(temp.graph)
+	N_edges<-ecount(temp.graph)
+	
+	g.components <- clusters(temp.graph)
+	N_clusters<-g.components$no
+	Max_csize<-max(g.components$csize)
+	Max_c_edges<-ecount(induced.subgraph(temp.graph, which(g.components$membership == which.max(g.components$csize))))
+        
+	g.density<-graph.density(temp.graph)
+	g.pathlength.avg<-average.path.length(temp.graph)
+	
+	betcent<-centralization.betweenness(temp.graph)$centralization
+	degcent<-centralization.degree(temp.graph)$centralization
+	
+	g.mod<-modularity(edge.betweenness.community(temp.graph))
+	
+	efsize_avg<-mean(effective.size(temp.graph, mode = "all"))
+	
+	clustering_coeff<-transitivity(temp.graph)
         
         rand<-replicate(1000, {
         	clustering_coeff_rand<-transitivity(erdos.renyi.game(length(V(temp.graph)),length(E(temp.graph)),type="gnm"))
@@ -41,14 +56,12 @@ for(i in unique(final_results$id)){
         rand_ci.975<-qt(.975, df=length(rand)-1)*(sd(rand)/sqrt(length(rand)))
                 
         cluster_ratio<-clustering_coeff/rand_avg
-        test<-cbind(id, clustering_coeff, rand_avg, rand_ci.975, cluster_ratio)
-        netword_clustering<-rbind(network_clustering, test)
+        test<-cbind(id, N_nodes, N_edges, N_clusters, Max_csize, Max_c_edges, g.density, g.pathlength.avg, betcent, degcent, g.mod, efsize_avg, clustering_coeff, rand_avg, rand_ci.975, cluster_ratio)
+        network_clustering<-rbind(network_clustering, test)
         print(paste("finished ", i ,sep=": "))
 }
-#network_clustering[, 2:5]<-lapply(network_clustering[, 2:5], as.character)
-#network_clustering[, 2:5]<-lapply(network_clustering[, 2:5], as.numeric)
 
-final_stat<-merge(barn_foaming.rate, netword_clustering, "id")
+final_stat<-merge(barn_foaming.rate, network_clustering, "id")
 
 # you can write the results out into a flat tab delimited table
 write.table(final_stat, paste(unlist(input_name)[1], "_final_stats.txt", sep=""), sep="\t", row.names=F, quote=F) 
