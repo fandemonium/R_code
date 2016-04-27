3c3b98e53aa55f7b8010ac596c01a97365d9b055
Fan Yang <snisiarc@gmail.com>
2016-04-26 21:38:15 -0500

    editted the final stat script

--- rscripts/spearman_cc/spearman_hoeffding_cc_final_stats.R
+++ rscripts/spearman_cc/spearman_hoeffding_cc_final_stats.R
@@ -13,8 +13,8 @@ input_name<-strsplit(as.character(full_name), ".", fixed=T)
 
 final_results<-read.delim(args[1], sep="\t", header=T)
 
-#barn_foaming.rate<-read.delim("barn_foaming_rate.txt", sep="\t", header=T)
-#barn_foaming.rate<-read.delim(args[2], sep="\t", header=T)
+##barn_foaming.rate<-read.delim("barn_foaming_rate.txt", sep="\t", header=T)
+barn_foaming.rate<-read.delim(args[2], sep="\t", header=T)
 #final_results<-merge(final_results, barn_foaming.rate[, c("id", "category")], "id")
 #final_results$Var1<-paste(final_results$Var1, final_results$category, sep="::")
 #final_results$Var2<-paste(final_results$Var2, final_results$category, sep="::")
@@ -25,20 +25,30 @@ final_results$Var2<-paste(final_results$Var2, final_results$id, sep="::")
 final_results<-data.frame(final_results[, c("Var1", "Var2", "id")], final_results[, 4:length(final_results[1,])])
 
 # now we can calculate stats for the network
-final_stats<-data.frame()
+network_clustering<-data.frame()
 for(i in unique(final_results$id)){
         temp<-subset(final_results, id==i)
         temp.graph<-(graph.edgelist(as.matrix(temp[,c(1,2)]),directed=FALSE))
         E(temp.graph)$weight<-abs(temp$rho)
         temp.graph<-simplify(temp.graph)
-        stats<-data.frame(row.names((as.matrix(igraph::degree(temp.graph,normalized=TRUE)))),(as.matrix(igraph::degree(temp.graph,normalized=TRUE))),(as.matrix(igraph::betweenness(temp.graph))))
-        names(stats)<-c("otus","norm_degree","betweenness")
-        stats$id <- i
-        stats$clustering_coeff<-igraph::transitivity(temp.graph,type="global")
-        stats$clustering_coeff_rand<-igraph::transitivity(igraph::erdos.renyi.game(length(V(temp.graph)),length(E(temp.graph)),type="gnm"))
-        stats$cluster_ratio<-stats$clustering_coeff/stats$clustering_coeff_rand
-        final_stats<-rbind(final_stats,stats)
+        id <- i
+        clustering_coeff<-transitivity(temp.graph)
+        
+        rand<-replicate(1000, {
+        	clustering_coeff_rand<-transitivity(erdos.renyi.game(length(V(temp.graph)),length(E(temp.graph)),type="gnm"))
+        	})
+        rand_avg<-mean(rand)
+        rand_ci.975<-qt(.975, df=length(rand)-1)*(sd(rand)/sqrt(length(rand)))
+                
+        cluster_ratio<-clustering_coeff/rand_avg
+        test<-cbind(id, clustering_coeff, rand_avg, rand_ci.975, cluster_ratio)
+        netword_clustering<-rbind(network_clustering, test)
         print(paste("finished ", i ,sep=": "))
 }
+#network_clustering[, 2:5]<-lapply(network_clustering[, 2:5], as.character)
+#network_clustering[, 2:5]<-lapply(network_clustering[, 2:5], as.numeric)
+
+final_stat<-merge(barn_foaming.rate, netword_clustering, "id")
+
 # you can write the results out into a flat tab delimited table
-write.table(final_stats, paste(unlist(input_name)[1], "_final_stats.txt", sep=""), sep="\t", row.names=F, quote=F) 
+write.table(final_stat, paste(unlist(input_name)[1], "_final_stats.txt", sep=""), sep="\t", row.names=F, quote=F) 
