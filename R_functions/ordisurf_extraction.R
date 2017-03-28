44558e71512da9c7bd0a44c5b195261bfec0f183
Fan Yang <snisiarc@gmail.com>
2017-03-28 16:37:12 -0500

    ggplot2 ellipse with ordisurf plot

--- R_functions/ordisurf_extraction.R
+++ R_functions/ordisurf_extraction.R
@@ -1,7 +1,13 @@
-ordi.sf<-function(nmds, env){
-
-env.sf<-ordisurf(nmds ~ env, plot=F, scaling=3)
-print(summary(env.sf))
+## nmds: nmds object
+## env:  2 column data frame; 1 column has header same as SAMPLE_ID, 1 column is the env variable
+## SAMPLE_ID: one of the env column name for matching
+ordi.sf<-function(nmds, env, SAMPLE_ID){
+	library(vegan)
+        NMDS<-data.frame(nmds$points)[, 1:2]
+        NMDS$SAMPLES<-row.names(NMDS)
+        NMDS.si<-merge(NMDS, env, by.x="SAMPLES", by.y=SAMPLE_ID)
+	env.sf<-ordisurf(NMDS.si[, c("MDS1", "MDS2")]~ env[, !names(env) %in% SAMPLE_ID], plot=F)
+	print(summary(env.sf))
 
 extract.xyz <- function(obj) {
     xy <- expand.grid(x = obj$grid$x, y = obj$grid$y)
@@ -11,5 +17,5 @@ extract.xyz <- function(obj) {
     return(xyz)
 }
 
-contour.vals <- extract.xyz(obj=env.sf)
+	contour.vals <- extract.xyz(obj=env.sf)
 }
