bd66728870d438bd916696b23d61070f448e7d62
Fan Yang <snisiarc@gmail.com>
2017-05-19 08:55:11 -0500

    modified the nmds ellipse to use factors with NAs

--- R_functions/ggplot_nmds_eclipse.R
+++ R_functions/ggplot_nmds_eclipse.R
@@ -1,13 +1,17 @@
 ##plot NMDS using ggplot modified from RJW's code
+## this NMDS with ellipse function will take experiment factors containing NA's 
+## and plot them using one big metaMDS output. 
+
 ggplot.NMDS.ellipse<-function(XX,ZZ,COLORS){
         library(ggplot2)
+	library(vegan)
 MDS1<-data.frame(scores(XX))$NMDS1
 MDS2<-data.frame(scores(XX))$NMDS2
 Treatment<-ZZ
 
 NMDS<-data.frame(MDS1,MDS2,Treatment)
+NMDS.narm <- subset(NMDS, !is.na(Treatment))
 
-NMDS.mean=aggregate(NMDS[,1:2],list(group=Treatment),mean)
 
 veganCovEllipse<-function (cov, center = c(0, 0), scale = 1, npoints = 100) 
   {
@@ -16,14 +20,14 @@ veganCovEllipse<-function (cov, center = c(0, 0), scale = 1, npoints = 100)
     t(center + scale * t(Circle %*% chol(cov)))
   }
 
-  df_ell <- data.frame()
-  for(g in levels(NMDS$Treatment)){
-    df_ell <- rbind(df_ell, cbind(as.data.frame(with(NMDS[NMDS$Treatment==g,],
+df_ell <- data.frame()
+for(g in levels(NMDS.narm$Treatment)){
+    df_ell <- rbind(df_ell, cbind(as.data.frame(with(NMDS.narm[NMDS.narm$Treatment==g,],
                     veganCovEllipse(cov.wt(cbind(MDS1,MDS2),wt=rep(1/length(MDS1),length(MDS1)))$cov,center=c(mean(MDS1),mean(MDS2)))))
                     ,group=g))
-  }
+}
 
-X1<-ggplot(data = NMDS, aes(MDS1, MDS2)) + geom_point(aes(color = Treatment),size=1.5,alpha=0.75) +
+X1<-ggplot(data = NMDS.narm, aes(MDS1, MDS2)) + geom_point(aes(color = Treatment),size=1.5,alpha=0.75) +
     geom_path(data=df_ell, aes(x=MDS1, y=MDS2,colour=group), size=2, linetype=5)+
     theme_classic()+ theme(axis.line.x = element_line(colour = 'black', size=1, linetype='solid'),axis.line.y = element_line(colour = 'black', size=1, linetype='solid')) +
     theme(aspect.ratio=1)+
